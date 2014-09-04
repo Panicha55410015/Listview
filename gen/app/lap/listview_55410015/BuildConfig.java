b25e1f26ae025662fccc471494292dee52680b72
Panicha55410015 <james5043@hotmail.com>
2014-09-04 12:04:28 +0700

    Listview

---
+++ gen/app/lap/listview_55410015/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package app.lap.listview_55410015;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/app/lap/listview_55410015/List_items.java
@@ -0,0 +1,23 @@
+package app.lap.listview_55410015;
+
+import android.graphics.drawable.Drawable;
+
+public class List_items {
+	private Drawable drawable;
+	private String title;
+	public Drawable getDrawable() {
+		return drawable;
+	}
+	public void setDrawable(Drawable daDrawable) {
+		this.drawable = daDrawable;
+	}
+	public String getTitle(){
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+		
+	}
+		
+	}
+
